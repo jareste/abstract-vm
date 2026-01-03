5026f81013ae763a2a1e20876484807cfc5fc0ec
Joan Areste <jareste2000@gmail.com>
2026-01-03 14:22:53 +0100

    refactored main and done

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -10,105 +10,111 @@
 #if !defined(TEST_OPERAND_MAIN)
 // #define PRINT_LINES
 
-int main(int argc, char** argv)
+namespace
 {
-    bool isStdin;
-    std::string filename;
-    size_t linesRead;
-    vm virtualMachine;
-    Instruction instr;
-    std::vector<Token> tokens;
-    Line line;
 
-    LOG("Hello, Abstract VM!");
+    constexpr size_t kBatchSize = 10000;
 
-    isStdin = (argc == 1);
-    filename = (isStdin) ? "stdin" : argv[1];
-
-    std::unique_ptr<inputReader> iput;
-    try
+    void printLine(const Line& line)
     {
-        iput = std::make_unique<inputReader>(filename, isStdin);
+    #ifdef PRINT_LINES
+        std::cout << "Line " << line.no << ": " << line.text << "\n";
+    #else
+        (void)line;
+    #endif
     }
-    catch (AVMException const& e)
+
+    std::unique_ptr<inputReader> makeInput(int argc, char** argv)
     {
-        std::cerr << e.what() << std::endl;
+        const bool isStdin = (argc == 1);
+        const std::string filename = isStdin ? "stdin" : argv[1];
+        return std::make_unique<inputReader>(filename, isStdin);
+    }
+
+    bool processLine(vm& virtualMachine, const Line& line)
+    {
+        printLine(line);
+
+        std::vector<Token> tokens = Lexer::tokenize(line);
+        Instruction instr = Parser::parseInstruction(tokens);
+
+        if (instr.op == OpCode::None)
+            return false;
+
+        if (instr.op == OpCode::Exit)
+        {
+            LOG("Exit instruction encountered. Exiting.");
+            return true;
+        }
+
+        virtualMachine.executeInstruction(instr);
+        return false;
+    }
+
+    bool runProgram(inputReader& input, vm& virtualMachine)
+    {
+        for (size_t linesRead = input.readProgram(kBatchSize); linesRead > 0; linesRead = input.readProgram(kBatchSize))
+        {
+            LOG("Read " << linesRead << " lines from input.");
+
+            for (Line line = input.getLine(); line.no != 0; line = input.getLine())
+            {
+                if (processLine(virtualMachine, line))
+                    return true;
+            }
+
+            LOG("End of lines.");
+        }
+        return false;
+    }
+
+    int reportAndFail(const char* prefix, const std::exception& e)
+    {
+        if (prefix && *prefix)
+            std::cerr << prefix << ": ";
+        std::cerr << e.what() << "\n";
         return 1;
     }
 
-    linesRead = iput->readProgram(10000);
-    while (linesRead > 0)
-    {
-        LOG("Read " << linesRead << " lines from input.");
-
-        line = iput->getLine();
-        LOG("Printing lines read:");
-        while ((line.no != 0))
-        {
-#ifdef PRINT_LINES
-            std::cout << "Line " << line.no << ": " << line.text << std::endl;
-#endif
-            try
-            {
-                tokens = Lexer::tokenize(line);
-            }
-            catch (LexicalError const& e)
-            {
-                std::cerr << e.what() << std::endl;
-                std::cerr << "Exiting due to lexical error." << std::endl;
-                return 1;
-            }
-
-            try
-            {
-                instr = Parser::parseInstruction(tokens);
-            }
-            catch (SyntaxError const& e)
-            {
-                std::cerr << e.what() << std::endl;
-                std::cerr << "Exiting due to syntax error." << std::endl;
-                return 1;
-            }
-
-            if (instr.op == OpCode::None)
-            {
-                line = iput->getLine();
-                continue;
-            }
-
-            if (instr.op == OpCode::Exit)
-            {
-                LOG("Exit instruction encountered. Exiting.");
-                return 0;
-            }
-
-            try
-            {
-                virtualMachine.executeInstruction(instr);
-            }
-            catch (AVMException const& e)
-            {
-                std::cerr << e.what() << std::endl;
-                std::cerr << "Exiting due to VM error." << std::endl;
-                return 1;
-            }
-            catch (std::exception const& e)
-            {
-                /* stoll */
-                std::cerr << "Standard exception: " << e.what() << std::endl;
-                std::cerr << "Exiting due to unexpected error." << std::endl;
-                return 1;
-            }
-
-
-            line = iput->getLine();
-        }
-
-        LOG("End of lines.");
-        linesRead = iput->readProgram(10000);
-    }
-
-    std::cerr << "No exit instruction found." << std::endl << "Exiting with error." << std::endl;
-    return 1;
 }
+
+int main(int argc, char** argv)
+{
+    vm virtualMachine;
+    
+    LOG("Hello, Abstract VM!");
+
+    try
+    {
+        std::unique_ptr<inputReader> input = makeInput(argc, argv);
+        const bool sawExit = runProgram(*input, virtualMachine);
+
+        if (!sawExit)
+        {
+            std::cerr << "No exit instruction found.\nExiting with error.\n";
+            return 1;
+        }
+        return 0;
+    }
+    catch (const LexicalError& e)
+    {
+        std::cerr << e.what() << "\nExiting due to lexical error.\n";
+        return 1;
+    }
+    catch (const SyntaxError& e)
+    {
+        std::cerr << e.what() << "\nExiting due to syntax error.\n";
+        return 1;
+    }
+    catch (const AVMException& e)
+    {
+        std::cerr << e.what() << "\nExiting due to VM error.\n";
+        return 1;
+    }
+    catch (const std::exception& e)
+    {
+        return reportAndFail("Unexpected std::exception", e);
+    }
+}
+
 #endif
